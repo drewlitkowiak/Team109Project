f445608dd8be0ddedf676cfd7ce3affd0a397b83
root <root@Warspite.localdomain>
2020-11-03 16:26:17 -0600

    commiting changes

--- mysite/selector/views.py
+++ mysite/selector/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def search(request):
-    return HttpResponse('<h1> Search for food items </h1>')
+    return render(request, 'selector/search.html')
 
 def edit(request):
-    return HttpResponse('<h1> edit database </h1>')
+    return render(request, 'selector/edit.html')
