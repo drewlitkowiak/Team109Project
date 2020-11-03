560dfc887e5ad7673f63eadd1f0690aed9a09978
Xin <xinhong3@illinois.edu>
2020-11-03 17:20:24 -0600

    create models

--- mysite/selector/models.py
+++ mysite/selector/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    userID = models.BigAutoField(primary_key = True)
+    userName = models.CharField(max_length=255)
+    userEmail = models.EmailField(max_length=255)
+    userZip = models.IntegerField(default=61820)
+
+    class Meta:
+        db_table = 'Users'
+
+    def __str__(self):
+        return str(self.userID) + '|' + self.userName
+
+class Restaurants(models.Model):
+    restaurantId = models.BigAutoField(primary_key=True)
+    restaurantName = models.CharField(max_length=255)
+    restaurantAddress = models.CharField(max_length=255)
+    restaurantZip = models.IntegerField(default=61820)
+
+    class Meta:
+        db_table = 'Restaurants'
+
+    def __str__(self):
+        return self.restaurantName
+
+class Ratings(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurants, on_delete=models.CASCADE)
+    ratingNum = models.IntegerField()
+
+    class Meta:
+        db_table = 'Ratings'
+
+
+class FoodItems(models.Model):
+    foodID = models.BigAutoField(primary_key=True)
+    restaurant = models.ForeignKey(Restaurants, on_delete=models.CASCADE)
+    cuisine = models.CharField(max_length=255)
+    foodName = models.CharField(max_length=255)
+    vegetarian = models.CharField(max_length=255)
+    allergies1 = models.CharField(max_length=255)
+    allergies2 = models.CharField(max_length=255)
+    price = models.FloatField()
+
+    class Meta:
+        db_table = 'FoodItems'
