1e5ecc79ae467343359b2297e562e3dc4d2bacb5
Drew Litkowiak <drewlitkowiak@Drews-MacBook-Air.local>
2020-11-05 18:25:15 -0600

    Allowed other hosts to connect to the server

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8fp6pe_)eom^6-@xzr*g!k45f7bm*agl=98+f_bq549gx7l^h7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
