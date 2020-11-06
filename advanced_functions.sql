99c72f3a610cdb96bc09a780a081f5e93a5c0d31
drewlitkowiak <drewlitkowiak@gmail.com>
2020-11-05 18:14:31 -0600

    Adding the advanced function as a sql file in the main directory

--- advanced_functions.sql
+++ advanced_functions.sql
@@ -1,2 +1,12 @@
+use food;
+
 SELECT R.restaurantName, count(F.FoodID) AS amountFood
-FROM 
+FROM Restaurants as R NATURAL JOIN FoodItems as F
+WHERE F.cuisine = 'American'
+GROUP BY R.restaurantId
+ORDER BY amountFood DESC;
+
+SELECT R.restaurantName, avg(F.price) as avgPrice
+FROM Restaurants AS R NATURAL JOIN FoodItems as F
+GROUP BY R.restaurantId
+ORDER BY avgPrice DESC
