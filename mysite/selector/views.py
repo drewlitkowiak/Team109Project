5f1088a768df53bf72aa9cb2c83166643df98de5
root <root@Warspite.localdomain>
2020-11-23 16:15:34 -0600

    testing neo4j

--- mysite/selector/views.py
+++ mysite/selector/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import UpdateFoodForm, DeleteFoodForm, InsertRestaurantForm, FoodPriceSelectorForm, InsertFoodForm, DeleteRestaurantForm, InsertRatingsForm, UpdatePrefForm, RecomenderForm
 from .models import Users, Restaurants, FoodItems, Ratings
+from .neomodel_models import FoodItems, Attributes, Restaurant, User
 from django.db import connection
 # Create your views here.
 
